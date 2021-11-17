36aad64cacc85a284e9362e6a09a8e4b90c9e717
Mourad Kejji <mkejji@gmail.com>
2021-11-17 02:44:50 +0100

    Set number with an input

--- web/roulette-web/src/app/app.module.ts
+++ web/roulette-web/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +14,8 @@ import { Web3Service } from './web3.service'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [Web3Service],
   bootstrap: [AppComponent]
